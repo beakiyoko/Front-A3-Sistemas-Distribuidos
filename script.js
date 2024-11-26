97129c03aa5be29eeec9728176515f6ac43ee881
beakiyoko <beakiyoko@gmail.com>
2024-11-26 20:12:54 -0300

    Commit inicial do front end da A3 de sistemas distribuidos

---
+++ script.js
@@ -0,0 +1,43 @@
+//relógio digital
+const relogio = document.querySelector(".relogio");
+
+function padZero(num) {
+    return String(num).padStart(2, "0");
+}
+
+function mostrarHora (){
+    const data = new Date();
+    const hora = padZero(data.getHours());
+    const minuto = padZero(data.getMinutes());
+    const segundo = padZero(data.getSeconds());   
+    relogio.innerHTML = hora + ":" + minuto + ":" + segundo;
+}
+
+mostrarHora();
+setInterval(() => {
+    mostrarHora();
+}, 1000)
+
+
+function pesquisar() {
+    alert("Função de pesquisa chamada!");
+}
+
+// botão de novo registro
+function registrarNovoHorario() {
+    const registroDiv = document.getElementById("registro-atual");    
+    const agora = new Date();
+    const dataFormatada = agora.toLocaleDateString("pt-BR");
+    const horaFormatada = agora.toLocaleTimeString("pt-BR");
+
+    registroDiv.innerText = `Último registro: ${dataFormatada} às ${horaFormatada}`;
+
+    console.log(`Novo registro feito: ${dataFormatada} às ${horaFormatada}`);
+}
+
+// botão de logout
+function logout() {
+    console.log("Logout efetuado.");
+    alert("Você foi deslogado.");
+    window.location.href = "/login.html";
+}
